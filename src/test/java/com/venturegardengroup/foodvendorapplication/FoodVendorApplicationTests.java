210b6ed9d2f3542bb48085a0dded62feb6d47cae
cyriacusifeanyi <cyriacusifeanyi@gmail.com>
2020-05-02 14:05:06 +0100

    Food Vendor Application - SpringBoot Initialization

---
+++ src/test/java/com/venturegardengroup/foodvendorapplication/FoodVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venturegardengroup.foodvendorapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodVendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
